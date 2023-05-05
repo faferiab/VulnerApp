e692584fe2993500143f36f4786062069445df04
FreddyFeria <faferiab@unal.edu.co>
2023-05-04 22:29:37 -0500

    Fix for text and labels

--- app/src/app/pages/map-page/utils/category-description.ts
+++ app/src/app/pages/map-page/utils/category-description.ts
@@ -3,17 +3,17 @@ import { Item } from '../interfaces/api-types';
 export function getDescription(category: Item) {
   switch (category.code) {
     case '01':
-      return 'El tiempo de viaje esta en minutos según Utam de origen';
+      return 'El tiempo de viaje está en minutos según la zona (UTAM) de origen';
     case '02':
-      return 'El tiempo de espera esta en minutos según Utam de origen';
+      return 'El tiempo de espera (cuánto tarda en llegar el bus) está en minutos según la zona (UTAM) de origen';
     case '03':
-      return 'El tiempo de acceso (caminata) esta en minutos según Utam de origen';
+      return 'El tiempo de acceso (caminata desde la casa a la estación / paradero) está en minutos según la zona (UTAM) de origen';
     case '04':
-      return 'Número de transferencias realizadas durante el viaje según Utam de origen';
+      return 'Número de transferencias (transbordos) realizadas durante el viaje según la zona (UTAM) de origen';
     case '05':
-      return 'Probabilidad de pagar un sobrecosto adicional a la tarifa del viaje según Utam de origen';
+      return 'Probabilidad de pagar un sobrecosto adicional a la tarifa del viaje según la zona (UTAM) de origen';
     case '06':
-      return 'Nivel de experiencia negativa donde 1, la experiencia de viaje es mejor y 5, la experiencia de viaje es peor';
+      return 'Nivel de experiencia negativa donde 0 es la mejor experiencia de viaje y 5 la peor experiencia de viaje';
     case '07':
       return 'Sobrecosto adicional en pesos COP a la tarifa del modo principal de transporte';
     case '08':
